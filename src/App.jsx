f5dc5fa5b0cf7b1eb2561cd380178c0eab609013
Chris Sauter <sauterdev@gmail.com>
2023-04-11 20:50:03 -0400

    useQuery implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Bumps" animal="Toad" breed="American Toad" />
-    <Pet name="Cutie" animal="Fish" breed="Siamese Fighting Fish" />
-  </div>;
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
